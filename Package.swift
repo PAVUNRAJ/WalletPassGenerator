b47b4720dd6f3ea73ac5ea39d197e4f90d889595
PAVUNRAJ <32295877+PAVUNRAJ@users.noreply.github.com>
2023-02-12 18:54:54 +0530

    version changed

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.3.0")
     ],
     targets: [
         .target(
