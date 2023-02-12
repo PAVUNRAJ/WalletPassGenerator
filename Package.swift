5341d23d75dcca5a499a8de57e3ed2ec41a52ee5
PAVUNRAJ <32295877+PAVUNRAJ@users.noreply.github.com>
2023-02-12 22:12:13 +0530

    removed version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/PAVUNRAJ/ShellOut.git", from: "2.3.0")
+        .package(url: "https://github.com/PAVUNRAJ/ShellOut.git")
     ],
     targets: [
         .target(
